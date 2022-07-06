f0a6aac11694a44650b07de9ce598944d68dd4eb
Markus Ast <m@rkusa.st>
2022-07-06 11:23:03 +0200

    upgrade to wasi sdk to v16

--- build-wasi-sdk.sh
+++ build-wasi-sdk.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 set -e
 cd wasm
-# `--branch wasi-sdk-14` doesn't build, so use `main` for now
-git clone --branch wasi-sdk-14 https://github.com/WebAssembly/wasi-sdk.git
+git clone --branch wasi-sdk-16 https://github.com/WebAssembly/wasi-sdk.git
 cd wasi-sdk
 git submodule update --init --recursive
 NINJA_FLAGS=-v make package
